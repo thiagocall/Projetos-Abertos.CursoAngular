485d7dbdd092cecfa37cc08299c1e8704e298f27
thiagocall <call.thiago@gmail.com>
2025-12-18 18:14:37 -0300

    Video 2 finalizado

--- src/app/initial/forms/forms.component.ts
+++ src/app/initial/forms/forms.component.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common'
 import {FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl, FormArray, AbstractControl, ValidationErrors} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { SendDataService } from './send-data.service';
+<<<<<<< Updated upstream
 import { debounceTime, filter, Observable, startWith } from 'rxjs';
+=======
+import { debounceTime, filter, startWith, tap } from 'rxjs';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-forms',
@@ -18,12 +22,19 @@ fb = inject(FormBuilder)
 dataService = inject(SendDataService)
 toastr = inject(ToastrService)
 
+<<<<<<< Updated upstream
 // $nomeChange: Observable<any>;
+=======
+fb = inject(FormBuilder);
+toastr = inject(ToastrService);
+sendDataServicce = inject(SendDataService);
+>>>>>>> Stashed changes
 
 constructor() {
 
      this.frm = this.fb.group(
     {
+<<<<<<< Updated upstream
       'nome':['',[Validators.minLength(3),Validators.required]],
       'enderecos': this.fb.array([ this.criarEnderecoControl()]),
       'email':['',[Validators.required, Validators.email, this.emailRegexValidator()]],
@@ -31,18 +42,53 @@ constructor() {
   )
 
      this.frm.controls['nome'].valueChanges
+=======
+      'nome':['',[Validators.required]],
+      'enderecos':this.fb.array([ this.criarEnderecoControl()]),
+      'email':['',[Validators.required, this.emailRegexValidator()]],
+    }
+  )
+
+  this.frm.controls['nome'].valueChanges
+>>>>>>> Stashed changes
     .pipe(
       startWith(''),
       debounceTime(700)
     )
     .subscribe(
+<<<<<<< Updated upstream
       res=>{
         console.log(res)
       }
     )
+=======
+      res => {console.log(res)}
+    )
+
+>>>>>>> Stashed changes
 }
 
+  criarEnderecoControl(): FormControl {
+    return this.fb.control('', [Validators.required, Validators.maxLength(30)]);
+  }
+
+  get enderecos(): FormArray {
+    return this.frm.get('enderecos') as FormArray;
+  }
+
+   adicionarEndereco(): void {
+    this.enderecos.push(this.criarEnderecoControl());
+  }
+
+    removerEndereco(index: any) {
+    this.enderecos.removeAt(index);
+    
+  }
+
+
+
 salvar(){
+<<<<<<< Updated upstream
   
   if(this.frm.invalid)
   {
@@ -50,6 +96,19 @@ salvar(){
     this.frm.markAllAsTouched()
     return;
   }
+=======
+  console.log('Estado do Formulário',this.frm.controls['email'].errors)
+
+  if(this.frm.invalid){
+    this.frm.markAllAsTouched()
+    return;
+  }
+
+  this.sendDataServicce.send(this.frm.getRawValue())
+  .subscribe(res=>{
+    console.log(res)
+  })
+>>>>>>> Stashed changes
 
   console.log('Estado do Formulário', this.frm.status)
 
@@ -59,9 +118,9 @@ salvar(){
 
       )
   this.toastr.success("Está bem!")
-
 }
 
+<<<<<<< Updated upstream
   get enderecos(): FormArray {
     return this.frm.get('enderecos') as FormArray;
   }
@@ -98,5 +157,20 @@ enviarEndereco() {
 
 }
 
+=======
+emailRegexValidator(){
+  const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/i;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value
+
+    if(!value) return null;
+
+    const isvalid = regex.test(value);
+
+    return isvalid? null : {emailInvalido: true}
+
+  }
+}
+>>>>>>> Stashed changes
 
 }
